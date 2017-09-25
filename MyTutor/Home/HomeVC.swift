58247a5640b24ac3daf6cf948f868dbc9420476b
Lin XU <linxumentor@gmail.com>
2017-09-25 16:05:36 -0400

    first commit

---
+++ MyTutor/Home/HomeVC.swift
@@ -0,0 +1,18 @@
+//
+//  HomeVC.swift
+//  MyTutor
+//
+//  Created by Lin XU on 2017-09-25.
+//  Copyright © 2017 linx. All rights reserved.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
